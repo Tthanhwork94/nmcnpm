0f8335c751bcca5b8ead8fcbb8a07f329afd368d
Phuoc <phuocvan27102002@gmail.com>
2022-11-27 10:01:06 +0700

    js cho dang nhap, dang ky

---
+++ login.js
@@ -0,0 +1,33 @@
+// check nhập
+$(function() {
+    $("form[name='dangky']").validate({
+      rules: {
+        matkhau: "required",
+        tendangnhap: "required",
+        xacnhanmatkhau: "required"
+      },
+      messages: {
+        matkhau: "Hãy mật khẩu hiện tại",
+        tendangnhap: "Hãy nhập tên đăng nhập",
+        xacnhanmatkhau: "Nhập lại mật khẩu"
+      },
+      submitHandler: function(form) {
+        form.submit();
+      }
+    });
+  });
+
+
+//   button đăng nhập
+  var btn = document.getElementById("dangnhap");
+btn.onclick = function () {
+  var check4 =document.getElementById("matkhau").value ;
+  var check5 =document.getElementById("tendangnhap").value ;
+  if (check4!="" && check5!="")
+  {
+    alert("Đăng nhập thành công");
+  }
+  
+}
+
+
